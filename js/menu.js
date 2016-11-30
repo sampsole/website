b16bde74d10d022455d16418d326737a7b4e2d0d
sampsole <sampsole@gmail.com>
2016-11-30 15:10:47 +1300

    testing a website

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+$(document).ready(enableMobileMenu);
+
+function enableMobileMenu(){
+  var menuButton = $('.menu-button');
+  var menu = $('header nav');
+
+  function toggleMenu(){
+    menu.toggleClass('hide-on-mobile');
+  }
+
+  menuButton.click(toggleMenu);
+}
